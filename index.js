865af4ae9c67bd1f58ca7843ab95d50cfc758f5f
orvi2014 <das.orvi@yahoo.com>
2017-02-04 18:51:38 +0600

    all files loaded and fixed

--- index.js
+++ index.js
@@ -48,4 +48,6 @@ io.sockets.on('connection', function (socket, username) {
 
 
 
-server.listen(8080);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
